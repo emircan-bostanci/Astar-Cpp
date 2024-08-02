e92876ba3fba57e6be24e18b78703d1122b434cb
Doğukan Şimşek <dogukan.simsek@blfoptik.com>
2024-08-02 17:58:28 +0300

    astar classes generated

--- Astar.cpp
+++ Astar.cpp
@@ -1,1 +1,23 @@
 #include "Astar.h"
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+
+
+Astar::Astar(Node * start,Node * goal,int borderx,int borderY) {
+
+}
+
+/// <summary>
+///	Finds optimal way according to Astar Search Algorithm 
+/// </summary>
+/// <returns>Return path with nodes</returns>
+Node* Astar::Find() {
+	while (this->openList.size() > 0) {
+		sort(this->openList.begin(), this->openList.end());
+
+	}
+
+}
--- Astar.h
+++ Astar.h
@@ -1,5 +1,18 @@
 #pragma once
+#include "Node.h"
+#include <vector>
+
+using namespace std;
 class Astar
 {
+private:
+	vector<Node*> openList;
+	vector<Node*> closedList;
+public:
+
+	Astar(Node * startNode,Node *  goalNode,int borderX,int borderY);
+	//Node Array
+	Node* Find();
+
 };
 
